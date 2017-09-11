0f82e028d30e9c71c0f5799858ce961440433494
DESKTOP-8N58AB9\Penda <p.tomlinson@mdx.ac.uk>
2017-09-11 13:50:55 +0100

    Starting with 01base

---
+++ 01Base/Assets/PT_Scripts/PT_basic_move.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PT_basic_move : MonoBehaviour {
+    public GameObject GO_bullet;
+    public float fl_Character_Rotation_Speed = 90;
+    public float fl_Character_Move_Speed = 5;
+    public float fl_CoolDownTime = 3;
+
+    //---
+    private Touch tc_FirstTouch;
+
+
+//---Private and used internally
+    private float fl_CoolDown; 
+    private Rigidbody2D RB_PC;
+    
+    // Use this for initialization
+    void Start () {
+        RB_PC = GetComponent<Rigidbody2D>();
+        fl_CoolDown = Time.time + fl_CoolDownTime; 
+    }
+
+    //---------------------------------------------------------------------------------------
+    // Update is called once per frame
+    void Update()
+    {
+        // Call the custom functions each frame
+        MovePC();
+        MoveTouch();
+        Attack();
+    }//----- 
+
+    //---------------------------------------------------------------------------------------
+    // Custom method for Character movement - Asteroid Style 
+    void MovePC()
+    {
+        // Rotate with H axis    
+        transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * fl_Character_Rotation_Speed * Time.deltaTime);
+
+        // Move with V axis     
+        RB_PC.velocity = transform.TransformDirection(Vector2.right) * fl_Character_Move_Speed * Input.GetAxis("Vertical");
+    }//-----
+
+    void MoveTouch()
+    {
+       tc_FirstTouch =  Input.touches[0];
+        // do some triangle maths to move towards touch?
+
+        // rubber band?
+
+    }
+
+    // Custom method  for triggering an attack
+    void Attack()
+    {
+        /*
+         * 
+         * 
+         * 
+         // spawn a bomb when B is pressed
+        if (Input.GetKeyDown("b") && in_bombs > 0)
+        {
+            Instantiate(GO_bomb, transform.position + transform.TransformDirection(Vector2.right), transform.rotation);
+            in_bombs--;
+        }
+         */
+
+        // Has the fire button (CTRL or mouse) been pressed and cooldown delay time passed
+        if (Input.GetButtonDown("Fire1") && Time.time > fl_CoolDown)
+        {
+            // create a bullet 1 unit in front of the PC if the front is the right hand side
+            Instantiate(GO_bullet, transform.position + transform.TransformDirection(Vector2.right ), transform.rotation);
+
+            // reset cooldown time
+            fl_CoolDown = Time.time + fl_CoolDownTime;
+        }
+        
+    }
+}
---
+++ 01Base/Assets/PT_Scripts/PT_bullet_move.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PT_bullet_move : MonoBehaviour {
+
+    [SerializeField]
+    private float fl_Bullet_Move_Speed = 5;
+    [SerializeField]
+    private float fl_Bullet_Range = 20;
+
+    //----
+    private Rigidbody2D RB_Bullet;
+
+    public float Fl_Bullet_Move_Speed
+    {
+        get
+        {
+            return fl_Bullet_Move_Speed;
+        }
+
+        set
+        {
+            fl_Bullet_Move_Speed = value;
+        }
+    }
+
+    public float Fl_Bullet_Range
+    {
+        get
+        {
+            return fl_Bullet_Range;
+        }
+
+        set
+        {
+            fl_Bullet_Range = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        RB_Bullet = GetComponent<Rigidbody2D>();
+        // Set velocity to move forwards at the speed defined above
+        RB_Bullet.velocity = transform.TransformDirection(Vector2.right) * Fl_Bullet_Move_Speed;
+
+        // Remove this object from the scene when the range is reached - 
+        Destroy(gameObject, Fl_Bullet_Range / Mathf.Abs(Fl_Bullet_Move_Speed));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
