5aae98c92db7a42bbab5537a75b57a5b04cb34be
Unknown <p.tomlinson@mdx.ac.uk>
2019-05-13 15:58:15 +0100

    small add

---
+++ Xtra_CardDeck/Assets/Scripts/Deck.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Deck : ScriptableObject {
+
+    public string DeckName = "new deck name";
+    public List<CardTemplate> CardList;
+
+
+}
---
+++ Xtra_CardDeck/Assets/Scripts/DeckManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckManager : MonoBehaviour {
+
+    //cards to be used
+    public Deck mainDeck;
+    //cards from last time or for next time
+    public Deck consequenceDeck;
+    //the deck in use
+    public List<CardTemplate> currentDeck;
+
+    public CardTemplate currentDrawnCard;
+
+    public GameObject CardUIManager;
+
+    //private 
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShuffleDeck()
+    {
+
+    }
+    
+    public void DrawNextCard()
+    {
+
+    }
+
+    public void EndOfDeck()
+    {
+
+    }
+}
