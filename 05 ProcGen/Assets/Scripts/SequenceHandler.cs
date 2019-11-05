7943707caf79309c44011d6f712c3be82c77efed
Penda <p.tomlinson@mdx.ac.uk>
2019-11-05 16:23:20 +0000

    sequence script outline

---
+++ 05 ProcGen/Assets/Scripts/SequenceHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceHandler : MonoBehaviour
+{
+    public GameObject[] currentSequence;
+
+    public GameObject[] allInteractables;
+
+    public int[,] allSequences;
+
+    private int interactionStage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Interacted(GameObject clickedObject)
+    {
+        if (currentSequence[interactionStage] == clickedObject)
+        {
+
+        }
+    }
+
+
+}
