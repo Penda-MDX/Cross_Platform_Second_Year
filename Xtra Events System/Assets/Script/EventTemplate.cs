1c321ffe1222b592681d5b5da6c61f94c01584af
Penda-MDX <p.tomlinson@mdx.ac.uk>
2022-10-24 21:30:06 +0100

    Xtra Events System

---
+++ Xtra Events System/Assets/Script/EventTemplate.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class EventTemplate : ScriptableObject
+{
+    public string Description;
+    public string Choice01;
+    public string Choice02;
+
+    public int Resource01_C01;
+    public int Resource01_C02;
+    public int Resource02_C01;
+    public int Resource02_C02;
+    public int Resource03_C01;
+    public int Resource03_C02;
+    public int Resource04_C01;
+    public int Resource04_C02;
+    public int Resource05_C01;
+    public int Resource05_C02;
+
+    public string Choice01_Outcome;
+    public string Choice02_Outcome;
+
+    public string Choice01_Special;
+    public string Choice02_Special;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Xtra Events System/Assets/Script/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int Resource01;
+    public int Resource02;
+    public int Resource03;
+    public int Resource04;
+    public int Resource05;
+
+    public List<EventTemplate> eventList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateEventList()
+    {
+
+    }
+}
