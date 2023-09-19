a76cd2c27e3dc43023d9002b4ba2b3ba06c9c1f6
Penda-MDX <p.tomlinson@mdx.ac.uk>
2023-09-19 14:14:14 +0100

    Unity Update and Reorg

---
+++ Dep 03 Present/Assets/PT_Scripts/PT_Six_Buttons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PT_Six_Buttons : MonoBehaviour {
+
+    /*
+    * 
+    *    Create a script that manages a list with 6 items in it and 6 buttons on screen and put words in some of the list entries. 
+    *    When the list entry has a word in it change the word on the button. When the list has nothing in it disable the button. 
+    * 
+    */
+
+       // Use this for initialization
+       void Start () {
+
+       }
+
+       // Update is called once per frame
+       void Update () {
+
+       }
+   }
---
+++ Dep 05 ProcGen/Assets/Scripts/Sequence_element.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sequence_element : MonoBehaviour
+{
+
+
+    public Material baseMatter;
+    public Material activatedMatter;
+    public Material clickedMatter;
+    public float changeCoolDown;
+
+    private float restTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (restTime < Time.time)
+        {
+            ChangeMaterial(baseMatter);
+        }
+
+    }
+ 
+    void OnMouseDown()
+    {
+        SetClicked();
+    }
+
+    public void SetActive()
+    {
+        ChangeMaterial(activatedMatter);
+    }
+
+    public void SetClicked()
+    {
+        ChangeMaterial(clickedMatter);
+    }
+
+    void ChangeMaterial(Material newMaterial)
+    {
+        gameObject.GetComponent<Renderer>().material = newMaterial;
+        if (newMaterial != baseMatter)
+        {
+            restTime = Time.time + changeCoolDown;
+        }
+    }
+}
