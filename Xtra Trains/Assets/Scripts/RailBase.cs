d3418d0f55958f011b05593b1bb45937f8a4a9c7
Penda-MDX <p.tomlinson@mdx.ac.uk>
2025-10-21 09:58:37 +0100

    Trains Thinking

---
+++ Xtra Trains/Assets/Scripts/RailBase.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEngine;
+
+public class RailBase : MonoBehaviour
+{
+    public GameObject start;
+    public GameObject middle;
+    public GameObject end;
+    
+    //gameobjects with 
+    public GameObject startConnect;
+    public GameObject endConnect;
+
+
+    public bool pause;
+    public float pauseTimeLength;
+
+
+
+
+    private float restartTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
